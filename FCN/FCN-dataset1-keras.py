c1c3f560c41b4842029ecb0d3e24b8eb7841334f
zhouhua <zhouhua@qq.com>
2019-11-18 16:47:35 +0800

    给代码添加注释

--- FCN/FCN-dataset1-keras.py
+++ FCN/FCN-dataset1-keras.py
@@ -104,52 +104,52 @@ def FCN8(nClasses,  input_height=224, input_width=224):
     img_input = Input(shape=(input_height, input_width, 3))  # Assume 224,224,3
 
     # Block 1
-    x = Conv2D(64, (3, 3), activation='relu', padding='same',
+    x = Conv2D(64, (3, 3), activation='relu', padding='same',           # 224 224 64
                name='block1_conv1', data_format=IMAGE_ORDERING)(img_input)
-    x = Conv2D(64, (3, 3), activation='relu', padding='same',
+    x = Conv2D(64, (3, 3), activation='relu', padding='same',           # 224 224 64
                name='block1_conv2', data_format=IMAGE_ORDERING)(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool',
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool',        # 112 112 64
                      data_format=IMAGE_ORDERING)(x)
     f1 = x
 
     # Block 2
-    x = Conv2D(128, (3, 3), activation='relu', padding='same',
+    x = Conv2D(128, (3, 3), activation='relu', padding='same',          # 112 112 128
                name='block2_conv1', data_format=IMAGE_ORDERING)(x)
-    x = Conv2D(128, (3, 3), activation='relu', padding='same',
+    x = Conv2D(128, (3, 3), activation='relu', padding='same',          # 112 112 128
                name='block2_conv2', data_format=IMAGE_ORDERING)(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool',
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool',        # 56 56 128
                      data_format=IMAGE_ORDERING)(x)
     f2 = x
 
     # Block 3
-    x = Conv2D(256, (3, 3), activation='relu', padding='same',
+    x = Conv2D(256, (3, 3), activation='relu', padding='same',              # 56 56 256
                name='block3_conv1', data_format=IMAGE_ORDERING)(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same',
+    x = Conv2D(256, (3, 3), activation='relu', padding='same',              # 56 56 256
                name='block3_conv2', data_format=IMAGE_ORDERING)(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same',
+    x = Conv2D(256, (3, 3), activation='relu', padding='same',              # 56 56 256
                name='block3_conv3', data_format=IMAGE_ORDERING)(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool',
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool',            # 28 28 256
                      data_format=IMAGE_ORDERING)(x)
     pool3 = x
 
     # Block 4
-    x = Conv2D(512, (3, 3), activation='relu', padding='same',
+    x = Conv2D(512, (3, 3), activation='relu', padding='same',              # 28 28 512
                name='block4_conv1', data_format=IMAGE_ORDERING)(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same',
+    x = Conv2D(512, (3, 3), activation='relu', padding='same',              # 28 28 512
                name='block4_conv2', data_format=IMAGE_ORDERING)(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same',
+    x = Conv2D(512, (3, 3), activation='relu', padding='same',              # 28 28 512
                name='block4_conv3', data_format=IMAGE_ORDERING)(x)
     pool4 = MaxPooling2D((2, 2), strides=(
         2, 2), name='block4_pool', data_format=IMAGE_ORDERING)(x)  # (None, 14, 14, 512)
 
     # Block 5
-    x = Conv2D(512, (3, 3), activation='relu', padding='same',
+    x = Conv2D(512, (3, 3), activation='relu', padding='same',              # 14 14 512
                name='block5_conv1', data_format=IMAGE_ORDERING)(pool4)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same',
+    x = Conv2D(512, (3, 3), activation='relu', padding='same',              # 14 14 512
                name='block5_conv2', data_format=IMAGE_ORDERING)(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same',
+    x = Conv2D(512, (3, 3), activation='relu', padding='same',              # 14 14 512
                name='block5_conv3', data_format=IMAGE_ORDERING)(x)
-    pool5 = MaxPooling2D((2, 2), strides=(
+    pool5 = MaxPooling2D((2, 2), strides=(                                  
         2, 2), name='block5_pool', data_format=IMAGE_ORDERING)(x)  # (None, 7, 7, 512)
 
     #x = Flatten(name='flatten')(x)
@@ -170,26 +170,26 @@ def FCN8(nClasses,  input_height=224, input_width=224):
     vgg.load_weights(VGG_Weights_path)
 
     n = 4096
-    o = (Conv2D(n, (7, 7), activation='relu', padding='same',
+    o = (Conv2D(n, (7, 7), activation='relu', padding='same',           # (None, 7, 7, 4096)  使用7x7卷积核，替换CNN结构的全连接层
                 name="conv6", data_format=IMAGE_ORDERING))(pool5)
-    conv7 = (Conv2D(n, (1, 1), activation='relu', padding='same',
+    conv7 = (Conv2D(n, (1, 1), activation='relu', padding='same',       # (None, 7, 7, 4096)  
                     name="conv7", data_format=IMAGE_ORDERING))(o)
 
     # 4 times upsamping for pool4 layer
-    conv7_4 = Conv2DTranspose(nClasses, kernel_size=(4, 4),  strides=(
+    conv7_4 = Conv2DTranspose(nClasses, kernel_size=(4, 4),  strides=(  # 4*(7-1)+4  (None, 28, 28, 12)
         4, 4), use_bias=False, data_format=IMAGE_ORDERING)(conv7)
     ## (None, 224, 224, 10)
     # 2 times upsampling for pool411
-    pool411 = (Conv2D(nClasses, (1, 1), activation='relu', padding='same',
+    pool411 = (Conv2D(nClasses, (1, 1), activation='relu', padding='same',      # pool4:(None, 14, 14, 512)   pool411:(None, 14, 14, 512)
                       name="pool4_11", data_format=IMAGE_ORDERING))(pool4)
-    pool411_2 = (Conv2DTranspose(nClasses, kernel_size=(2, 2),  strides=(
+    pool411_2 = (Conv2DTranspose(nClasses, kernel_size=(2, 2),  strides=(       # pool4ll_2: (None, 28, 28, 12)
         2, 2), use_bias=False, data_format=IMAGE_ORDERING))(pool411)
 
-    pool311 = (Conv2D(nClasses, (1, 1), activation='relu', padding='same',
+    pool311 = (Conv2D(nClasses, (1, 1), activation='relu', padding='same',      # pool3:(None, 28, 28, 256)  pool3ll: (None, 28, 28, 12) 
                       name="pool3_11", data_format=IMAGE_ORDERING))(pool3)
-
-    o = Add(name="add")([pool411_2, pool311, conv7_4])
-    o = Conv2DTranspose(nClasses, kernel_size=(8, 8),  strides=(
+    # 这里就是论文中提到的FCN8，即pool3+2倍上采样的pool4+4倍上采样。
+    o = Add(name="add")([pool411_2, pool311, conv7_4])                          # (None, 28, 28, 12) add ((None, 28, 28, 12) add (None, 28, 28, 12)
+    o = Conv2DTranspose(nClasses, kernel_size=(8, 8),  strides=(                # 28*8 (None, 224, 224, 12)
         8, 8), use_bias=False, data_format=IMAGE_ORDERING)(o)
     o = (Activation('softmax'))(o)
 
@@ -236,17 +236,17 @@ if __name__ == '__main__':
     X, Y = readImgaeAndSeg()
     (X_train, y_train), (X_test, y_test) = splitDatasets(X, Y, 0.85)
 
-    # 保存测试数据，在jupyter中导入使用模型预测，观察效果
-    pickle_out = open("X_test.pickle", "wb")
-    pickle.dump(X_test, pickle_out)
-    pickle_out.close()
+    # # 保存测试数据，在jupyter中导入使用模型预测，观察效果
+    # pickle_out = open("X_test.pickle", "wb")
+    # pickle.dump(X_test, pickle_out)
+    # pickle_out.close()
 
-    pickle_out = open("y_test.pickle", "wb")
-    pickle.dump(y_test, pickle_out)
-    pickle_out.close()
+    # pickle_out = open("y_test.pickle", "wb")
+    # pickle.dump(y_test, pickle_out)
+    # pickle_out.close()
 
-    tensorboard = TensorBoard(
-        log_dir='/home/ye/zhouhua/logs/FCN/FCN-dataset1-keras-{}'.format(int(time.time())))
+    # tensorboard = TensorBoard(
+    #     log_dir='/home/ye/zhouhua/logs/FCN/FCN-dataset1-keras-{}'.format(int(time.time())))
 
     model = FCN8(nClasses=n_classes,
                  input_height=224,
