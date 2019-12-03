16b85c80ee175ac0cb427bac0b1b9d9bebc20497
zhouhua <zhouhua25@qq.com>
2019-12-03 17:27:33 +0800

    修改路径,去除多余代码

--- FCN-dataset1-keras.py
+++ FCN-dataset1-keras.py
@@ -15,26 +15,11 @@ import pickle
 
 if __name__ == '__main__':
     commonUtils.GPUConfig()
-    if (os.path.exists("./data/X.pickle") and os.path.exists("./data/Y.pickle")):
-        X = pickle.load('./data/X.pickle')
-        Y = pickle.load('./data/Y.pickle')
-        
-    else:
-        dataGen = dataset1_Utils()
-        X, Y = dataGen.readImgaeAndSeg()
-        # # 保存测试数据，在jupyter中导入使用模型预测，观察效果
-        pickle_out = open("./data/X.pickle", "wb")
-        pickle.dump(X, pickle_out)
-        pickle_out.close()
-
-        pickle_out = open("./data/Y.pickle'", "wb")
-        pickle.dump(Y, pickle_out)
-        pickle_out.close()
-    
+    dataGen = dataset1_Utils()
+    X, Y = dataGen.readImgaeAndSeg()
     (X_train, y_train), (X_test, y_test) = dataGen.splitDatasets(X, Y, 0.85)
-    
     tensorboard = TensorBoard(
-         log_dir='/home/ye/zhouhua/logs/FCN/FCN-dataset1-keras-{}'.format(int(time.time())))
+         log_dir='./logs/FCN/FCN-dataset1-{}'.format(time.strftime('%Y-%m-%d_%H_%M_%S', time.localtime())))
 
     model = FCN8(nClasses=12,
                  input_height=224,
@@ -46,12 +31,12 @@ if __name__ == '__main__':
                   optimizer=sgd,
                   metrics=['accuracy']
                   )
-    best_weights_filepath = './models/best_weights.hdf5'
+    best_weights_filepath = './models/FCN_best_weights.hdf5'
     earlyStopping=EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode='auto')
     saveBestModel = ModelCheckpoint(best_weights_filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
     hist1 = model.fit(X_train, y_train,
                       validation_data=(X_test, y_test),
                       batch_size=32, epochs=200, verbose=1, callbacks=[tensorboard, earlyStopping, saveBestModel])
-    #reload best weights
-    #model.load_weights(best_weights_filepath)
+    with open('./data/FCN-dataset1-keras.py.txt', 'wb') as file_pi:
+        pickle.dump(hist1.history, file_pi)
     
