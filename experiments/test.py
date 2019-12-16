56082e9e11cd825a8ac758202760e50b8bc7f4d5
zhouhua <zhouhua25@qq.com>
2019-12-16 15:50:45 +0800

    更新仓库文件夹

--- experiments/test.py
+++ experiments/test.py
@@ -1,28 +1,6 @@
-        img = cv2.imread(path, 1)
-        img = np.float32(cv2.resize(img, ( width , height ))) / 127.5 - 1
-        return img
-
-def getSegmentationArr( path , nClasses ,  width , height  ):
-
-    seg_labels = np.zeros((  height , width  , nClasses ))
-    img = cv2.imread(path, 1)
-    img = cv2.resize(img, ( width , height ))
-    img = img[:, : , 0]
-
-    for c in range(nClasses):
-        seg_labels[: , : , c ] = (img == c ).astype(int)
-    ##seg_labels = np.reshape(seg_labels, ( width*height,nClasses  ))
-    return seg_labels
-images = os.listdir(dir_img)
-images.sort()
-segmentations  = os.listdir(dir_seg)
-segmentations.sort()
+class test:
+    def __init__(self, var1):
+        print(var1)
     
-X = []
-Y = []
-for im , seg in zip(images,segmentations) :
-    X.append( getImageArr(dir_img + im , input_width , input_height )  )
-    Y.append( getSegmentationArr( dir_seg + seg , n_classes , output_width , output_height )  )
 
-X, Y = np.array(X) , np.array(Y)
-print(X.shape,Y.shape)
+class1 = test('jjjjj')
