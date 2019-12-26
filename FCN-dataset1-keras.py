4f4b8717cc4a667c5b23211783f87385256ea52c
caijilun <1586567393@qq.com>
2019-12-26 14:33:20 +0800

    add lr(poly_decay，step-decay)

--- FCN-dataset1-keras.py
+++ FCN-dataset1-keras.py
@@ -5,15 +5,16 @@ import os
 import cv2
 import tensorflow as tf
 
-from keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, LearningRateScheduler
 from keras import optimizers
 from utils.utils import dataset1_Utils, commonUtils
 from models.FCN import FCN8
 from config import VGG_Weights_path
 import pickle
+from  utils.learning_rate import poly_decay, step_decay
 
 if __name__ == '__main__':
-    commonUtils.GPUConfig(gpu_device="1")           
+    commonUtils.GPUConfig(gpu_device="0")           
     dataGen = dataset1_Utils()
     X, Y = dataGen.readImgaeAndSeg()
     (X_train, y_train), (X_test, y_test) = dataGen.splitDatasets(X, Y, 0.85)
@@ -29,7 +30,8 @@ if __name__ == '__main__':
                  VGG_Weights_path=VGG_Weights_path)
     model.summary()
 
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=1, mode='auto', epsilon=0.0001, cooldown=0, min_lr=0)
+    #reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=1, mode='auto', epsilon=0.0001, cooldown=0, min_lr=0)
+    learning_rate_scheduler = tf.keras.callbacks.LearningRateScheduler(poly_decay(lr=1e-2, max_epochs=100))
     sgd = optimizers.SGD(lr=1E-2, decay=5**(-4), momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy',
                   optimizer=sgd,
@@ -46,7 +48,7 @@ if __name__ == '__main__':
         
     hist1 = model.fit(X_train, y_train,
                       validation_data=(X_test, y_test),
-                      batch_size=32, epochs=300, verbose=1, callbacks=[tensorboard, earlyStopping, saveBestModel, reduce_lr])
+                      batch_size=32, epochs=300, verbose=1, callbacks=[tensorboard, earlyStopping, saveBestModel, learning_rate_scheduler])
     with open('./data/FCN-dataset1-keras.pickle', 'wb') as file_pi:
         pickle.dump(hist1.history, file_pi)
     
